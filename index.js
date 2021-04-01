dff5d0beefc2b58a75dbf7cef9a994eba98845f5
Treee <itsatreee.twitch@gmail.com>
2021-04-01 11:55:29 -0600

    dynamic counters based on an object complete. need to determine the game mechanics

--- index.js
+++ index.js
@@ -3,21 +3,40 @@ const gameProperties = {
   mainCounter: 0,
   elements: {
     hydrogen: {
+      id: "element-hydrogen",
+      displayName: "Hydrogen",
       count: 0,
-      formula: (multiply, additive, dt) => {
-        return multiply * dt + dt * additive;
-      },
+      multiplier: 1,
+      additive: 0,
     },
     helium: {
+      id: "element-helium",
+      displayName: "Helium",
       count: 0,
+      multiplier: 1,
+      additive: 0,
     },
   },
 };
 let mainGameLoopInterval;
 
-function increaseHydrogen(dt) {
-  const increase = gameProperties.elements.hydrogen.formula(1, 1, dt);
-  gameProperties.elements.hydrogen.count += increase;
+function FofX(multiplier, additive, x) {
+  return multiplier * x + additive * x;
+}
+
+function increaseElement(elementKey, dt) {
+  gameProperties.elements[elementKey].count += FofX(
+    gameProperties.elements[elementKey].multiplier,
+    gameProperties.elements[elementKey].additive,
+    dt
+  );
+}
+
+function increaseElementsLoop(dt) {
+  const elements = Object.keys(gameProperties.elements);
+  for (let i = 0; i < elements.length; i++) {
+    increaseElement(elements[i], dt);
+  }
 }
 
 function attachStartLoopButton() {
@@ -33,11 +52,18 @@ function startGameLoop() {
 }
 
 function mainGameLoop(delta) {
-  increaseHydrogen(delta / 1000);
   gameProperties.mainCounter += 1;
+  increaseElementsLoop(delta / 1000);
+  updateAllUICounters();
+}
+
+function updateAllUICounters() {
   updateUICounter("main-counter", gameProperties.mainCounter);
-  updateUICounter("element-hydrogen", gameProperties.elements.hydrogen.count);
-  // console.log(`Increment ${gameProperties.mainCounter++}`);
+  const elements = Object.keys(gameProperties.elements);
+  for (let i = 0; i < elements.length; i++) {
+    const element = gameProperties.elements[elements[i]];
+    updateUICounter(element.id, element.count);
+  }
 }
 
 function updateUICounter(id, counter) {
@@ -54,7 +80,45 @@ function stopGameLoop() {
   clearInterval(mainGameLoopInterval);
 }
 
+function buildElementCounter(elementId, displayName, elementCounterValue) {
+  const counterWrapper = document.createElement("div");
+
+  const btnUpgrade = document.createElement("button");
+  btnUpgrade.setAttribute("id", `btn-${elementId}`);
+  btnUpgrade.innerHTML = displayName;
+  counterWrapper.append(btnUpgrade);
+  btnUpgrade.addEventListener("click", function () {});
+
+  // const counterLabel = document.createElement("label");
+  // counterLabel.setAttribute("id", `label-${elementId}`);
+  // counterLabel.setAttribute("for", `${elementId}`);
+  // counterLabel.innerHTML = `${displayName}: `;
+  // counterWrapper.append(counterLabel);
+
+  const newCounter = document.createElement("span");
+  newCounter.setAttribute("id", elementId);
+  newCounter.innerHTML = elementCounterValue;
+  counterWrapper.append(newCounter);
+
+  document.getElementById("main-counter-display").append(counterWrapper);
+}
+
+function buildElmentCounterDisplay() {
+  buildElementCounter(
+    "main-counter",
+    "Elapsed Time",
+    gameProperties.mainCounter
+  );
+  const elements = Object.keys(gameProperties.elements);
+  for (let i = 0; i < elements.length; i++) {
+    const element = gameProperties.elements[elements[i]];
+    // console.log(element);
+    buildElementCounter(element.id, element.displayName, element.count);
+  }
+}
+
 window.addEventListener("load", function () {
   attachStartLoopButton();
   attachStopLoopButton();
+  buildElmentCounterDisplay();
 });
